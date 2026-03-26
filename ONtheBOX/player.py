a1e88a44780cf0dd195dd4a3a7ed0e47d6cb1299
RachitLochan <149613941+RachitLochan@users.noreply.github.com>
2026-03-27 00:10:21 +0530

    Enhance Player class with sprite animations
    Refactor Player class to include sprite animation and improved movement handling.

--- ONtheBOX/player.py
+++ ONtheBOX/player.py
@@ -6,36 +6,63 @@ pygame.init()
 class Player(pygame.Rect):
     #pygame.draw.rect(screen, color, rect)and pygame.Rect diffrece todo->dynamically find size of image and 
     # put accodingly even option toscale
+
+    def loadspritesheet(self,path,framefat,frameheight):
+        sheet=pygame.image.load(path).convert_alpha()
+        frames=[]
+
+        fulllen= sheet.get_width()
+        fullwidth=sheet.get_height()
+        for i in range(0,fulllen,framefat):
+            if i + framefat <= fulllen:  #stupid condition for overflow
+                frame = sheet.subsurface((i, 0, framefat, frameheight))
+                frames.append(frame)
+
+        return frames
+
+
     def __init__(self,gamewindow,startx,starty,fat,tall,tiles):
         self.gamewindow=gamewindow
         self.tiles=tiles
         
         BASE_DIR = os.path.dirname(__file__)
         # player_L=pygame.image.load(image)
-        self.imageR = pygame.image.load(os.path.join(BASE_DIR, "caracter", "greenboy.png"))
+        self.walk_frames = self.loadspritesheet(os.path.join(BASE_DIR, "caracter", "walk.png"), 32,32)
+        self.jump_frames = self.loadspritesheet(os.path.join(BASE_DIR, "caracter", "jumpframe.png"), 20, 35)
+
+        self.imageR = pygame.image.load(os.path.join(BASE_DIR, "caracter", "guy.png"))
         self.imageL = pygame.transform.flip(self.imageR, True, False)
         self.image = self.imageR
         self.direction = "left"
 
-        # self.player_L=pygame.transform.scale(player_L,(fat,tall))
-        # self.player_R = pygame.transform.flip(player_L, True, False)
-        # self.direction = "left"
+        self.idle_frame = pygame.image.load(os.path.join(BASE_DIR, "caracter", "guy.png")).convert_alpha()
+
+        # Flip versions for animation some advance python code must undestand more
+        self.walk_frames_L = [pygame.transform.flip(f, True, False) for f in self.walk_frames]
+        self.jump_frames_L = [pygame.transform.flip(f, True, False) for f in self.jump_frames]
+
+        # Animationstate
+        self.state = "idle"
+        self.frame_index = 0
+        self.animation_speed = 0.2
+
+        self.image = self.idle_frame
+        self.direction = "right"
+        
         self.vel_x = 0
         self.vel_y = 0
         pygame.Rect.__init__(self,startx,starty,fat,tall)
 
-        # self.image=player_L
-
-        # if RATIO !=None:
-            # self.image=pygame.transform.scale_by(self.image,RATIO) 
-
+        #todo remove the player as in idle already passed him/her
     
     def update_direction(self,speed):
+        #is this neeeded now ? todo justv see
 
         if self.direction == "right":
             self.image = self.imageR
         elif self.direction == "left":
             self.image = self.imageL
+
     def collision(self):
         # horizontal
         self.x += self.vel_x
@@ -62,17 +89,30 @@ class Player(pygame.Rect):
         keys=pygame.key.get_pressed()
         
         self.vel_x = 0
+        moving=False
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.direction = "right"
+            moving=True
             self.x=max(0,self.x-speed)
         # if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             # self.y=min(self.y+speed,self.gamewindow.get_height()-self.height)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.direction = "left"
+            moving=True
             self.x=min(self.x+speed,self.gamewindow.get_width()-self.width)
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.jump()
-#swarnim part  
+            moving=True
+
+        # STATE LOGIC for which spritesheet to choose in animation bro
+        if self.vel_y != 0:
+            self.state = "jump"
+        elif moving:
+            self.state = "walk"
+        else:
+            self.state = "idle"
+
+
 
     def jump(self):
         # Only jump if standing on a tile or ground
@@ -94,12 +134,37 @@ class Player(pygame.Rect):
         if self.bottom >= self.gamewindow.get_height():
             self.bottom = self.gamewindow.get_height()
             self.vel_y = 0
+
+
     def draw(self):
-        self.gamewindow.blit(self.image,(self.topleft))
+        # SELECT ANIMATION
+        if self.state == "walk":
+            frames = self.walk_frames if self.direction == "right" else self.walk_frames_L
+        elif self.state == "jump": #need to edit as jump i sjust 1 image now todo
+            frames = self.jump_frames if self.direction == "right" else self.jump_frames_L
+        else:
+            self.image = self.idle_frame
+            self.gamewindow.blit(self.image, self.topleft)
+            return
+
+        # ANIMATE logic still uncler gotta study more nad again
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(frames): #modulo if over 8 go to 0 type shit
+            self.frame_index = 0
+
+        self.image = frames[int(self.frame_index)] #decimal to whole number
+
+        self.gamewindow.blit(self.image, self.topleft)
 """must find some way to find x and y position to update it and 
  in rect it was as simple as rect.x nad rect.y to find x and y
  lol turns out sisnce i inherted rect so i can directly call rect.x lol 
- yahooooooo"""
+ yahooooooo
+ 
+
+ #def draw(self):
+     #   self.gamewindow.blit(self.image,(self.topleft))
+ old simple draw is gone now animation wala draw will come
+ """
 
 
        
